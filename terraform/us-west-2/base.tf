4d99b5037365bbfff2c7c5bfd3ab058b25a0aacd
JP <joseph.perry@ca.com>
2018-01-09 14:19:56 -0700

    Terraform added, plaintext secrets added to .gitignore

---
+++ terraform/us-west-2/base.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  access_key = #
+  secret_key = #
+  region = "us-west-2"
+}
+
+resource "aws_instance" "dev-practice-02-jp" {
+  ami = "ami-8803e0f0"
+  instance_type = "t2.micro"
+  network_interface {
+    network_interface_id = "eni-f69184c6"
+    device_index = 0
+  }
+  tags {
+    Name = "dev-practice-02-jp"
+  }
+}
