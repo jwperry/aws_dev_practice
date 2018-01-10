1a7968e0f9986447693e0940e9412bdfacebe63d
JP <joseph.perry@ca.com>
2018-01-10 15:00:38 -0700

    Adds Terraform to create aws_network_interface

--- terraform/us-west-2/base.tf
+++ terraform/us-west-2/base.tf
@@ -1,7 +1,16 @@
 provider "aws" {
-  access_key = "../../secrets/plaintext/aws/terraform_dev_access_key"
-  secret_key = "../../secrets/plaintext/aws/terraform_dev_secret_key"
+  access_key = "${trimspace(file("../../secrets/plaintext/aws/terraform_dev_access_key"))}"
+  secret_key = "${trimspace(file("../../secrets/plaintext/aws/terraform_dev_secret_key"))}"
   region = "us-west-2"
+}
+
+resource "aws_network_interface" "dev-practice-02-jp-network-interface" {
+  subnet_id = "subnet-3baf2473"
+  private_ips = ["198.18.21.0"]
+  tags {
+    Name = "dev-practice-02-jp-network-interface",
+    Owner = "JP"
+  }
 }
 
 resource "aws_instance" "dev-practice-02-jp" {
@@ -12,6 +21,7 @@ resource "aws_instance" "dev-practice-02-jp" {
     device_index = 0
   }
   tags {
-    Name = "dev-practice-02-jp"
+    Name = "dev-practice-02-jp",
+    Owner = "JP"
   }
 }
