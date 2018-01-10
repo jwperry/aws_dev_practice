508ee9a8fd635b63cd68af6dce6f70431079980f
JP <joseph.perry@ca.com>
2018-01-10 15:17:23 -0700

    Tries removing network_interface creation in favor of direct subnet link

--- terraform/us-west-2/base.tf
+++ terraform/us-west-2/base.tf
@@ -4,22 +4,23 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_network_interface" "dev-practice-02-jp-network-interface" {
-  subnet_id = "subnet-3baf2473"
-  private_ips = ["198.18.21.0"]
-  tags {
-    Name = "dev-practice-02-jp-network-interface",
-    Owner = "JP"
-  }
-}
+# resource "aws_network_interface" "dev-practice-02-jp-network-interface" {
+#   subnet_id = "subnet-3baf2473"
+#   private_ips = ["198.18.21.0"]
+#   tags {
+#     Name = "dev-practice-02-jp-network-interface",
+#     Owner = "JP"
+#   }
+# }
 
 resource "aws_instance" "dev-practice-02-jp" {
   ami = "ami-8803e0f0"
   instance_type = "t2.micro"
-  network_interface {
-    network_interface_id = "eni-f69184c6"
-    device_index = 0
-  }
+  subnet_id = "subnet-3baf2473"
+  # network_interface {
+  #   network_interface_id = "eni-f69184c6"
+  #   device_index = 0
+  # }
   tags {
     Name = "dev-practice-02-jp",
     Owner = "JP"
