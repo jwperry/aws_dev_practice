1a00bdc0e45f776b5472ce512c56a00b880158d6
JP <joseph.perry@ca.com>
2018-02-05 16:46:37 -0700

    Updates Python install commands

--- terraform/us-west-2/base.tf
+++ terraform/us-west-2/base.tf
@@ -123,7 +123,7 @@ resource "aws_instance" "dev-practice-jp" {
 sudo apt-get update
 DEBIAN_FRONTEND=noninteractive sudo -E apt-get -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confnew' upgrade
 sudo apt update
-sudo apt install python2.7 -y
+sudo apt install python -y
 echo dev-practice-jp-0${count.index + 1} | sudo tee /etc/hostname > /dev/null
 sudo reboot
 EOF
